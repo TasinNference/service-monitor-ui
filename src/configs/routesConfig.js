78d22868a83a15a738fca4a7ac022c2fd992258d
Tasin Ahmed <tahmed@nference.net>
2023-08-10 19:30:20 -0400

    Add config

--- src/configs/routesConfig.js
+++ src/configs/routesConfig.js
@@ -15,13 +15,37 @@ const routes = {
             },
             scanner1: {
               label: 'Scanner 1',
-              machine_name: "S1",
+              machine_name: 'S1',
               dashboard: 'a94fd44b-6111-4881-b6a4-c3d014507a7b'
-            },
+            }
           }
-        },
+        }
       }
-    },
+    }
+  },
+  SystemTesting: {
+    cms: {
+      label: 'CMS',
+      machine_name: 'CMS',
+      dashboard: 'ae659ee8-d380-4db0-a341-0747f7e1e27a',
+      children: {
+        cluster1: {
+          label: 'Cluster 1',
+          children: {
+            cops: {
+              label: 'COPS',
+              machine_name: 'CS001',
+              dashboard: 'd2c31728-2ae1-48b7-b586-9236c3353757'
+            },
+            scanner1: {
+              label: 'Scanner 1',
+              machine_name: 'C1S1',
+              dashboard: 'a94fd44b-6111-4881-b6a4-c3d014507a7b'
+            }
+          }
+        }
+      }
+    }
   }
 };
 
