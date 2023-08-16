c2ac82b993fa846b9eb9279c2cdbe9fb5f1dd56c
Pramana <pramana>
2023-08-16 16:57:23 -0500

    Change influx url

--- src/configs/queryApi.js
+++ src/configs/queryApi.js
@@ -1,7 +1,7 @@
 import { InfluxDB } from '@influxdata/influxdb-client';
 
 const queryApi = new InfluxDB({
-  url: 'http://localhost:8086',
+  url: 'http://cms.pramana.com:8086',
   token: process.env.REACT_APP_INFLUX_TOKEN
 }).getQueryApi('pramana');
 
